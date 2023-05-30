1db5ccd2d07a5d0373340ae8e9c63559e0fbd3fe
harika penukonda <penukondaharika@gmail.com>
2023-05-30 13:53:28 -0500

    Update REST service to throw exception is studentId is not found

--- src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.demo.rest;
 import com.luv2code.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +34,29 @@ public class StudentRestController {
     public List<Student> getStudents(){
         return theStudent;
     }
+
+    // define endpoint pr "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        // just index into the list... keep it simple for now
+
+        // check the studentId again list size
+        if ((studentId >= theStudent.size()) || studentId<0){
+            throw new StudentNotFoundException("Student Id not found " + studentId);
+        }
+        return theStudent.get(studentId); // Use StudentId for the index
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
