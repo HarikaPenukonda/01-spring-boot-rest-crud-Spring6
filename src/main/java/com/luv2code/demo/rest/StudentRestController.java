6dddafc641d579801a82f53525829b3a9550c20e
harika penukonda <penukondaharika@gmail.com>
2023-05-30 15:20:06 -0500

    Refactor REST service... remove exceptional handling code

--- src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -47,34 +47,7 @@ public class StudentRestController {
         return theStudent.get(studentId); // Use StudentId for the index
     }
 
-    // Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse>handleException(StudentNotFoundException e){
 
-        // create a studentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(e.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-    }
-
-    // Add another exception handler... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse>handleException(Exception e){
-
-        // create a studentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(e.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-
-    }
 
 
 
