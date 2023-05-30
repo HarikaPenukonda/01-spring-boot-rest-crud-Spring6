313f4bbd912f332e8daef199bb308555d2a27a1b
harika penukonda <penukondaharika@gmail.com>
2023-05-30 15:08:50 -0500

    Adding Generic Exceptional Handler

--- src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -61,6 +61,21 @@ public class StudentRestController {
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
 
+    // Add another exception handler... to catch any exception (catch all)
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse>handleException(Exception e){
+
+        // create a studentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+
+    }
+
 
 
 
